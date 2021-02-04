e3ead044ea0d894c9323ebd1c79c337f9100407a
WildfootW <wildfootw@wildfoo.tw>
2021-02-04 23:30:42 +0800

    update other/usart.c

--- src/other/usart.c
+++ src/other/usart.c
@@ -4,23 +4,31 @@
  * Distributed under terms of the MIT license.
  */
 
+//#define F_CPU  1000000UL
+//#define F_CPU  2000000UL
+//#define F_CPU  4000000UL
+#define F_CPU  8000000UL
+//#define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 int main(void)
 {
     CLKPR = 0b10000000;
 
-//    CLKPR = 0b00000100; // 16 MHz / 16
-//#define F_CPU  1000000UL
-
-    CLKPR = 0b00000000; // 16 MHz / 1
-#define F_CPU 16000000UL
+//    CLKPR = 0b00000100; //  1 MHz
+//    CLKPR = 0b00000011; //  2 MHz
+//    CLKPR = 0b00000010; //  4 MHz
+    CLKPR = 0b00000001; //  8 MHz
+//    CLKPR = 0b00000000; // 16 MHz
 
     /*
+     *  1 MHz - 9600/  6/ 7.5, 115200/  0/84.3
+     *  2 MHz - 9600/ 12/ 0.2, 115200/  0/ 7.8
+     *  4 MHz - 9600/ 25/ 0.2, 115200/  1/ 7.8
+     *  8 MHz - 9600/ 51/ 0.2, 115200/  3/ 7.8
      * 16 MHz - 9600/103/ 0.2, 115200/  8/ 3.7
-     * 1  MHz - 9600/  6/ 7.5, 115200/  0/84.3
      */
-    UBRR0 = 103;
+    UBRR0 = 51;
     UCSR0C |= 1<<UCSZ01 | 1<<UCSZ00; // normal mode, int clk
     UCSR0B |= 1<<TXEN0; // enable transmit
     while (true){

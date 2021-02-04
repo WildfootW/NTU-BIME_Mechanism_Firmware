c949b76726c27ba8d4f478de42477b1639a41a4e
WildfootW <wildfootw@wildfoo.tw>
2021-02-04 23:36:03 +0800

    modify frequency

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,8 @@ USART usart(9600);
 void initial()
 {
     CLKPR = (1 << CLKPCE); // clock prescaler change enable
-    CLKPR = 0b00000100; // set clk to 1 Mhz (16 / 16)
+  //CLKPR = 0b00000100; // set clk to 1 Mhz (16 / 16)
+    CLKPR = 0b00000000; // set clk to 16 Mhz (16 / 1)
 
     usart.initial();
 }
